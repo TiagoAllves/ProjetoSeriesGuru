005199449a4b6712d43824b335563f186e1fe942
TiagoAllves <alvesmata04@outlook.com>
2013-11-21 16:22:38 -0200

    Alteraçao da tabela

--- ProjetoSeriesGuru.Web/Models/FiltarrExercicioModel.cs
+++ ProjetoSeriesGuru.Web/Models/FiltarrExercicioModel.cs
@@ -11,7 +11,7 @@ namespace ProjetoSeriesGuru.Web.Models
     {
         public int IdGrupamento { get; set; }
         public string Nome { get; set; }
-        public int Titulo { get; set; }
+        public int Grupamento { get; set; }
         public string Url { get; set; }
     }
 
@@ -20,7 +20,6 @@ namespace ProjetoSeriesGuru.Web.Models
         public IList<ExercicioModel> Exercicios { get; set; }
 
         public IList<GrupamentoModel> TodosGrupamentos { get; set; }
-
 
     }
 }
